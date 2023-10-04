853e56c5e5d798dac7196f8fa63a2589ff090921
Fabian Heuberger <fabian.heuberger@axonivy.com>
2023-10-04 16:52:08 +0200

    fixed format to match axonivy standard

--- threema-connector-test/src_test/threema/connector/test/MsgHanlderTest.java
+++ threema-connector-test/src_test/threema/connector/test/MsgHanlderTest.java
@@ -2,31 +2,27 @@ package threema.connector.test;
 
 import static apitool.MsgHandler.*;
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
-
 
 public class MsgHanlderTest {
 
-	  @Test
-	  public void generateKeys() {
-		byte[][] keys = genKeyPair();	
-		assertThat(keys.length).isEqualTo(2);
-		  
-		byte[] privKey = keys[0];
-		byte[] pubKey = keys[1];
-		assertThat(privKey).isNotEmpty();
-		assertThat(pubKey).isNotEmpty();
-	  }
-	  
-	  @Test
-	  public void encryptMsg(){
-		String plainMsg = "Hello World";
-		byte[][] keys = genKeyPair();	
-		byte[] privKey = keys[0];
-		byte[] pubKey = keys[1];
-		String encryptedMsg = encryptMessage(plainMsg, privKey, pubKey);
-		assertThat(encryptedMsg).isNotBlank();
-	  }
+  @Test
+  public void generateKeys() {
+    byte[][] keys = genKeyPair();
+    assertThat(keys.length).isEqualTo(2);
+    byte[] privKey = keys[0];
+    byte[] pubKey = keys[1];
+    assertThat(privKey).isNotEmpty();
+    assertThat(pubKey).isNotEmpty();
+  }
 
+  @Test
+  public void encryptMsg() {
+    String plainMsg = "Hello World";
+    byte[][] keys = genKeyPair();
+    byte[] privKey = keys[0];
+    byte[] pubKey = keys[1];
+    String encryptedMsg = encryptMessage(plainMsg, privKey, pubKey);
+    assertThat(encryptedMsg).isNotBlank();
+  }
 }
