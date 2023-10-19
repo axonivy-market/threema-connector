2c7ac39612c1360d37724d49aac1d7d15c538c85
Fabian Heuberger <fabian.heuberger@axonivy.com>
2023-10-19 13:43:55 +0200

    finished api mocking and changed tests to fit mock api

--- threema-connector-test/src_test/threema/connector/test/process/SingleRecipientTest.java
+++ threema-connector-test/src_test/threema/connector/test/process/SingleRecipientTest.java
@@ -4,6 +4,7 @@ package threema.connector.test.process;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
@@ -11,16 +12,28 @@ import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.environment.AppFixture;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.threema.mocks.ThreemaServiceMock;
 import threema.connector.receiverData;
 
-@IvyProcessTest
+@IvyProcessTest(enableWebServer = true)
 public class SingleRecipientTest {
 
 	private static final BpmProcess SINGLE_RECEIVER_PROCESS = BpmProcess.name("singleRecipient");
-	private static final String VALID_ID = "ECHOECHO";
+	private static final String VALID_ID = "validId";
 	private static final String INVALID_EMAIL = "invalid@mail.ch";
 	private static final String INVALID_PHONE = "41000000000";
 	private static final String MESSAGE = "Hello World";
+	private static final String PRIVATE_KEY = "ff364c727068fd6e3e6a711918393fa37649d902402a8eb31af108e79f625d82";
+	
+	@BeforeEach
+	void setup(AppFixture fixture) {
+		Ivy.var().set("connector.privateKey", PRIVATE_KEY);
+		Ivy.var().set("connector.secret", "secret");
+		Ivy.var().set("connector.threemaId", "threemaId");
+		fixture.config("RestClients.ThreemaGateway.Url", ThreemaServiceMock.URI);
+	}
 	
 	@Test
 	void sendMessageToValidSingleRecipientById(BpmClient bpmClient) {
@@ -39,7 +52,7 @@ public class SingleRecipientTest {
 		receiverData resultData = result.data().last();
 		String apiStatus = resultData.getApiResponse();
 		
-		assertThat(apiStatus).contains("404");
+		assertThat(apiStatus).isEqualTo("ID-Lookup: 404");
 	}
 	
 	@Test
@@ -49,7 +62,7 @@ public class SingleRecipientTest {
 		receiverData resultData = result.data().last();
 		String apiStatus = resultData.getApiResponse();
 		
-		assertThat(apiStatus).contains("404");
+		assertThat(apiStatus).isEqualTo("ID-Lookup: 404");
 	}
 	
 }
